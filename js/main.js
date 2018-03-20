0c267b3bb351e7fc97dfe064781fa228680bf812
shaggy611 <sevczik11@gmail.com>
2018-03-20 04:14:34 +0200

    sdfg

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready (function() {
     $(window).scroll(function (){
         if ($(window).scrollTop() < 1300) {
-           $('#own_iframe').attr("src", "https://www.youtube.com/embed/5SgyFaBS7WI?&controls=2&fs=0&autoplay=1");
+           $('#own_iframe').attr("src", "https://www.youtube.com/embed/c8GXs_eb1sY?&controls=2&fs=0&autoplay=1");
         }
 
          if ($(window).scrollTop() > 3000) {
            $('#own_iframe').removeAttr("src");
-           $('#own_iframe').attr("src", "https://www.youtube.com/embed/5SgyFaBS7WI?&controls=2&fs=0&autoplay=0");
+           $('#own_iframe').attr("src", "https://www.youtube.com/embed/c8GXs_eb1sY?&controls=2&fs=0&autoplay=0");
         }
     })
 
